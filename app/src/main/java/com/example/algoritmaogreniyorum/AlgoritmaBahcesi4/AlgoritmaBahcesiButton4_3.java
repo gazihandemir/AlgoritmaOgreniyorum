77cf378fc9b62b7d83eb66015b9d86b9e0469f54
gazih <gazihand@gmail.com>
2019-12-22 16:00:30 +0300

    22.12

--- app/src/main/java/com/example/algoritmaogreniyorum/AlgoritmaBahcesi4/AlgoritmaBahcesiButton4_3.java
+++ app/src/main/java/com/example/algoritmaogreniyorum/AlgoritmaBahcesi4/AlgoritmaBahcesiButton4_3.java
@@ -10,25 +10,27 @@ import android.widget.TextView;
 
 import com.example.algoritmaogreniyorum.R;
 
-public class AlgoritmaBahcesiButton4_2 extends AppCompatActivity {
+public class AlgoritmaBahcesiButton4_3 extends AppCompatActivity {
     Button btnDevam;
     TextView tvCevap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_algoritma_bahcesi_button4_2);
-        btnDevam = findViewById(R.id.btnDevamAlgo4_2);
+        setContentView(R.layout.activity_algoritma_bahcesi_button4_3);
+        btnDevam = findViewById(R.id.btnDevamAlgo4_3);
         btnDevam.setVisibility(View.INVISIBLE);
-        tvCevap = findViewById(R.id.tvCevapAlgo4_2);
+        tvCevap=findViewById(R.id.tvCevap4_3);
         tvCevap.setVisibility(View.INVISIBLE);
+
     }
-    public void calistirAlgo4_2(View view) {
+    public void calistirAlgo4_3(View view){
         tvCevap.setVisibility(View.VISIBLE);
-        tvCevap.setText("3 sayıdir ");
+        tvCevap.setText(" 5 ");
         btnDevam.setVisibility(View.VISIBLE);
     }
-    public void devamAlgo4_2(View view){
-        Intent intent = new Intent(getApplicationContext(),AlgoritmaBahcesiButton4_3.class);
+    public void devamAlgo4_3(View view){
+        Intent intent = new Intent(getApplicationContext(),AlgoritmaBahcesiButton4_4.class);
         startActivity(intent);
     }
+
 }
---
+++ app/src/main/java/com/example/algoritmaogreniyorum/AlgoritmaBahcesi4/AlgoritmaBahcesiButton4_4.java
@@ -0,0 +1,73 @@
+package com.example.algoritmaogreniyorum.AlgoritmaBahcesi4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.algoritmaogreniyorum.AlgoritmaBahcesiActivity;
+import com.example.algoritmaogreniyorum.R;
+
+public class AlgoritmaBahcesiButton4_4 extends AppCompatActivity {
+    Button btnDevam;
+    EditText edCevap;
+    static String cevap = "55";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_algoritma_bahcesi_button4_4);
+        btnDevam = findViewById(R.id.btnDevamAlgo4_4);
+        btnDevam.setVisibility(View.INVISIBLE);
+        edCevap = findViewById(R.id.edCevapAlgo4_4);
+
+    }
+
+    public void calistirAlgo4_4(View view) {
+        if (cevap.contentEquals(edCevap.getText().toString())){
+            LayoutInflater inflater = getLayoutInflater();
+            View layout = inflater.inflate(R.layout.toast_layout,
+                    (ViewGroup) findViewById(R.id.custom_toast_container));
+
+            TextView text = layout.findViewById(R.id.text);
+            text.setText("TEBRİKLER ! ");
+            text.setTextSize(36);
+            text.setTextColor(Color.parseColor("#ffffff"));
+
+            Toast toast = new Toast(getApplicationContext());
+            toast.setGravity(Gravity.CENTER, 0, 0);
+            toast.setDuration(Toast.LENGTH_LONG);
+            toast.setView(layout);
+            toast.show();
+            btnDevam.setVisibility(View.VISIBLE);
+        }else{
+            LayoutInflater inflater = getLayoutInflater();
+            View layout = inflater.inflate(R.layout.toast_layout2,
+                    (ViewGroup) findViewById(R.id.custom_toast_container));
+
+            TextView text = layout.findViewById(R.id.text);
+            text.setText("Yanlis Cevap!");
+            text.setTextSize(24);
+
+            Toast toast = new Toast(getApplicationContext());
+            toast.setGravity(Gravity.BOTTOM, 0, 0);
+            toast.setDuration(Toast.LENGTH_SHORT);
+            toast.setView(layout);
+            toast.show();
+        }
+    }
+
+    public void devamAlgo4_4(View view) {
+        Intent intent = new Intent(getApplicationContext(), AlgoritmaBahcesiActivity.class);
+        startActivity(intent);
+    }
+}
