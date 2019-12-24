0461f096c2a00d0a4e1cdbe3b493045c8ddbfdeb
gazih <gazihand@gmail.com>
2019-12-24 21:59:23 +0300

    24.12

--- app/src/main/java/com/example/algoritmaogreniyorum/AlgoritmaBahcesi8/AlgoritmaBahcesiButton8_2.java
+++ app/src/main/java/com/example/algoritmaogreniyorum/AlgoritmaBahcesi8/AlgoritmaBahcesiButton8_2.java
@@ -2,15 +2,39 @@ package com.example.algoritmaogreniyorum.AlgoritmaBahcesi8;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.algoritmaogreniyorum.R;
 
 public class AlgoritmaBahcesiButton8_2 extends AppCompatActivity {
+    Button btnDevam;
+    TextView tvCevap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_algoritma_bahcesi_button8_2);
+        btnDevam = findViewById(R.id.btnDevamAlgo8_2);
+        btnDevam.setVisibility(View.INVISIBLE);
+        tvCevap = findViewById(R.id.tvCevapAlgo8_2);
+        tvCevap.setVisibility(View.INVISIBLE);
+
+    }
+
+    public void calistirAlgo8_2(View view) {
+        tvCevap.setVisibility(View.VISIBLE);
+        tvCevap.setText("1");
+        btnDevam.setVisibility(View.VISIBLE);
+
+    }
+
+    public void devamAlgo8_2(View view) {
+        Intent intent = new Intent(getApplicationContext(), AlgoritmaBahcesiButton8_3.class);
+        startActivity(intent);
     }
 }
+
