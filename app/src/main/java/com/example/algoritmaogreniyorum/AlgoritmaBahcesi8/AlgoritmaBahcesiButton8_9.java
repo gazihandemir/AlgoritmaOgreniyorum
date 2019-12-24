80647556d401d3945606ba71f7e5f294610a317b
gazih <gazihand@gmail.com>
2019-12-24 22:43:16 +0300

    24.12

---
+++ app/src/main/java/com/example/algoritmaogreniyorum/AlgoritmaBahcesi8/AlgoritmaBahcesiButton8_9.java
@@ -0,0 +1,16 @@
+package com.example.algoritmaogreniyorum.AlgoritmaBahcesi8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.algoritmaogreniyorum.R;
+
+public class AlgoritmaBahcesiButton8_9 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_algoritma_bahcesi_button8_9);
+    }
+}
