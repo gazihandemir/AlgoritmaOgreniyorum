fc7f4c6981e159b44bc4e85d32c8a385bf9637b4
gazih <gazihand@gmail.com>
2019-12-27 13:14:44 +0300

    27.12

--- app/src/main/java/com/example/algoritmaogreniyorum/AlgoritmaBahcesi10/AlgoritmaBahcesiButton10_6.java
+++ app/src/main/java/com/example/algoritmaogreniyorum/AlgoritmaBahcesi10/AlgoritmaBahcesiButton10_6.java
@@ -2,8 +2,17 @@ package com.example.algoritmaogreniyorum.AlgoritmaBahcesi10;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.algoritmaogreniyorum.OgrenimSecimActivity;
 import com.example.algoritmaogreniyorum.R;
 
 public class AlgoritmaBahcesiButton10_6 extends AppCompatActivity {
@@ -12,5 +21,78 @@ public class AlgoritmaBahcesiButton10_6 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_algoritma_bahcesi_button10_6);
+
+
     }
+
+    public void cevapAlgo10_6_1(View view) {
+        LayoutInflater inflater = getLayoutInflater();
+        View layout = inflater.inflate(R.layout.toast_layout2,
+                (ViewGroup) findViewById(R.id.custom_toast_container));
+
+        TextView text = layout.findViewById(R.id.text);
+        text.setText("Tekrar deneyin ! ");
+        text.setTextSize(24);
+
+        Toast toast = new Toast(getApplicationContext());
+        toast.setGravity(Gravity.BOTTOM, 0, 0);
+        toast.setDuration(Toast.LENGTH_SHORT);
+        toast.setView(layout);
+        toast.show();
+    }
+
+    public void cevapAlgo10_6_2(View view) {
+
+        LayoutInflater inflater = getLayoutInflater();
+        View layout = inflater.inflate(R.layout.toast_layout2,
+                (ViewGroup) findViewById(R.id.custom_toast_container));
+
+        TextView text = layout.findViewById(R.id.text);
+        text.setText("Yanlis Cevap!");
+        text.setTextSize(24);
+
+        Toast toast = new Toast(getApplicationContext());
+        toast.setGravity(Gravity.BOTTOM, 0, 0);
+        toast.setDuration(Toast.LENGTH_SHORT);
+        toast.setView(layout);
+        toast.show();
+    }
+
+    public void cevapAlgo10_6_3(View view) {
+        LayoutInflater inflater = getLayoutInflater();
+        View layout = inflater.inflate(R.layout.toast_layout2,
+                (ViewGroup) findViewById(R.id.custom_toast_container));
+
+        TextView text = layout.findViewById(R.id.text);
+        text.setText("Emin misin !  ");
+        text.setTextSize(24);
+
+        Toast toast = new Toast(getApplicationContext());
+        toast.setGravity(Gravity.BOTTOM, 0, 0);
+        toast.setDuration(Toast.LENGTH_SHORT);
+        toast.setView(layout);
+        toast.show();
+    }
+
+    // true
+    public void cevapAlgo10_6_4(View view) {
+        LayoutInflater inflater = getLayoutInflater();
+        View layout = inflater.inflate(R.layout.toast_layout,
+                (ViewGroup) findViewById(R.id.custom_toast_container));
+
+        TextView text = layout.findViewById(R.id.text);
+        text.setText("TEBRİKLER ! ");
+        text.setTextSize(36);
+        text.setTextColor(Color.parseColor("#ffffff"));
+
+        Toast toast = new Toast(getApplicationContext());
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setView(layout);
+        toast.show();
+        Intent intent = new Intent(getApplicationContext(), OgrenimSecimActivity.class);
+        startActivity(intent);
+    }
+
+
 }
